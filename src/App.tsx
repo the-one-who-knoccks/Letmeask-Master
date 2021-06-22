bfbedd6b59b699297eaaacea36b23be63ae410d2
the-one-who-knoccks <thiagoalves.devp@gmail.com>
2021-06-22 17:44:51 -0300

    :recycle: Refactored the code

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { NewRoom } from './pages/NewRoom';
 import { HomePage } from './pages/HomePage';
 
+import { AuthContextProvider } from './contexts/AuthContext';
+
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" component={HomePage} exact />
-      <Route path="/rooms/news" component={NewRoom} />
+      <AuthContextProvider>
+        <Route path="/" component={HomePage} exact />
+        <Route path="/rooms/news" component={NewRoom} />
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
