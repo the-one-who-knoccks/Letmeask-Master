4eb7e80cc150d9e96d1bc024690aba28674de1f2
the-one-who-knoccks <thiagoalves.devp@gmail.com>
2021-06-22 13:42:31 -0300

    :truck: Added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+import { NewRoom } from './pages/NewRoom';
 import { HomePage } from './pages/HomePage';
 
 function App() {
-  return <HomePage />;
+  return (
+    <BrowserRouter>
+      <Route path="/" component={HomePage} exact />
+      <Route path="/rooms/news" component={NewRoom} />
+    </BrowserRouter>
+  );
 }
 
 export default App;
